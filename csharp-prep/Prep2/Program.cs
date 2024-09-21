2fe05e3a05656d9aa1caacc75ed8a6b6076027fc
nasbf <mo0ma@hotmail.com>
2024-09-21 10:02:56 -0500

    update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+         Console.Write("Write your grade: ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+
+        string letterGrade = " ";
+
+        if (grade <= 60)
+        {
+            letterGrade = ("F");
+        }
+        else if (grade <= 70)
+        {
+            letterGrade = ("D");
+        }
+        else if (grade <= 80)
+        {
+            letterGrade = ("C");
+        }
+        else if (grade <= 90)
+        {
+            letterGrade = ("B");
+        }
+        else
+        {
+            letterGrade = ("A Congratulations");
+        }
+        Console.WriteLine($"Your grade is {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You have pass the class Good job.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you can do better next time.");
+        }
     }
 }
